e062ed7b3cdd82c54161542bc0480b1f3cd663e4
skycode <sikiruyaya1@gmail.com>
2023-09-27 19:31:49 -0700

    Return a promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
 export default function getResponseFromAPI() {
   const success = true;
   return new Promise((resolve, reject) => {
-    if (!success) {
-      reject();
-    } else {
+    if (success) {
       resolve();
+    } else {
+      reject();
     }
   });
 }
