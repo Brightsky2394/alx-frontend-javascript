01a6cb9e599d83e23396b4491303ef58ae50036d
sikiru yaya <sikiruyaya1@gmail.com>
2023-10-03 11:55:04 +0000

    returns an array of ids from a list of object.

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,7 @@
 export default function getListStudentIds(arr) {
-  let newArr = [];
   if (arr instanceof Array) {
-    newArr = arr.map((item) => item.id);
+    return arr.map((item) => item.id);
+  } else {
+    return [];
   }
-
-  return newArr;
 }
-
