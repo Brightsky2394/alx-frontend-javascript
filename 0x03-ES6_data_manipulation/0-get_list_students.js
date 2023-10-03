0ca4d0be922b475b9e92e157b773b2888f30fb76
sikiru yaya <sikiruyaya1@gmail.com>
2023-10-03 09:19:53 +0000

    returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+export default function getListStudents() {
+const first = {
+	id: 1,
+	firstName: 'Guillaume',
+	location: 'San Francisco',
+};
+const second = {
+	id: 2,
+	firstName: 'James',
+	location: 'Columbia',
+};
+const third = {
+	id: 5,
+	firstName: 'Serena',
+	location: 'San Francisco',
+}
+
+const res = [first, second, third];
+return res;
+}
